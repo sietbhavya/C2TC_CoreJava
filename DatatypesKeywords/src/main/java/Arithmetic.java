54ec956cf6e1bf02652e6f48003934806daf7152
sietbhavya <bhavyahp02@gmail.com>
2022-08-24 13:32:45 +0530

    First Commit

---
+++ DatatypesKeywords/src/main/java/Arithmetic.java
@@ -0,0 +1,10 @@
+
+public class Arithmetic {
+	public static void main(String[] args)
+	{
+		int x=10, y=20;
+		System.out.println(x+y);
+		System.out.println(--x);
+		System.out.println(++y);
+	}
+}
---
+++ DatatypesKeywords/src/main/java/ifelse.java
@@ -0,0 +1,15 @@
+
+public class ifelse {
+	public static void main(String[]args)
+	{
+		int x=5;
+	if(x!=5)
+	{
+		System.out.println("Value of x is not 5");
+	}
+	else
+	{
+		System.out.println("Value of x is 5");
+	}
+	}
+}
